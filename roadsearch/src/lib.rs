f4318fb93d7ba305831ed25d9255fb899eaff125
Jakub <jakub@Lenovo-JV.aveco.com>
2015-12-09 11:32:40 +0100

    * fixed map format * struct for map

--- roadsearch/src/lib.rs
+++ roadsearch/src/lib.rs
@@ -3,28 +3,31 @@ pub mod map {
 #[derive(Debug,Clone,PartialEq)]
 pub struct Node {
   name: String,
+  id: i32,
   x: i32,
   y: i32,
 }
 
 impl Node {
-  pub fn new(name: &str, x: i32, y: i32) -> Node {
+  pub fn new(name: &str, id: i32, x: i32, y: i32) -> Node {
     Node {
       name: name.to_string(),
+      id: id,
       x: x,
       y: y,
     }
   }
-  
+
   pub fn parse(line: &str) -> Node {
-    let mut splited = line.split(" ");
+    let mut splited = line.split(", ");
     Node {
-      name: splited.next().unwrap().to_string(),
+      id: splited.next().unwrap().parse::< i32 >().unwrap(),
       x: splited.next().unwrap().parse::< i32 >().unwrap(),
       y: splited.next().unwrap().parse::< i32 >().unwrap(),
+      name: splited.next().unwrap().to_string(),
     }
   }
-  
+
   pub fn distance(&self, node: &Node) -> f32 {
     if self.x == node.x {
       return (self.y - node.y).abs() as f32;
@@ -33,6 +36,33 @@ impl Node {
       return (self.x - node.x).abs() as f32;
     }
     return (((self.x - node.x).pow(2) + (self.y - node.y).pow(2)) as f32).sqrt();
+  }
+}
+
+use std::collections;
+
+pub struct RoadMap {
+  nodes: Vec< Node >,
+  incidency_matrix: collections::HashMap< u32, u32 >,
+}
+
+impl RoadMap {
+  pub fn parse(file: &str) -> RoadMap {
+    let empty = file.find("\n\n").unwrap() + 1;
+    let lines = file.split_at(empty);
+    let mut nodes: Vec< Node > = Vec::new();
+    let mut  incidency_matrix: collections::HashMap< u32, u32 > = collections::HashMap::new();
+    for line in lines.0.split("\n") {
+      nodes.push(Node::parse(line));
+    }
+    for line in lines.1.split("\n") {
+      let record = line.split(", ");
+
+    }
+    RoadMap {
+      nodes: nodes,
+      incidency_matrix: incidency_matrix,
+    }
   }
 }
 
--- roadsearch/tests/lib.rs
+++ roadsearch/tests/lib.rs
@@ -4,8 +4,8 @@ use roadsearch::map::Node;
 
 #[test]
 fn parse_test() {
-  let expected = Node::new("Praha", 150, 60);
-  let instance = Node::parse("Praha 150 60");
+  let expected = Node::new("Praha", 1, 150, 60);
+  let instance = Node::parse("1, 150, 60, Praha");
   assert_eq!(expected, instance);
 }
 
@@ -17,9 +17,9 @@ fn parse_failed_test() {
 
 #[test]
 fn distance_test() {
-  let node1 = Node::new("Praha", 160, 60);
-  let node2 = Node::new("Brno", 200, 30);
-  let node3 = Node::new("Olomouc", 160, 30);
+  let node1 = Node::new("Praha", 1, 160, 60);
+  let node2 = Node::new("Brno", 2, 200, 30);
+  let node3 = Node::new("Olomouc", 3, 160, 30);
   assert_eq!(50 as f32, node1.distance(&node2));
   assert_eq!(0 as f32, node1.distance(&node1));
   assert_eq!(30 as f32, node1.distance(&node3));
